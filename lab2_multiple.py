a519cc04784b7fffcd8b6a3a4516c649c6b35202
GKenar <kenar.gf@gmail.com>
2019-02-22 20:08:40 +0200

    Добавлен вывод графика

--- lab2_multiple.py
+++ lab2_multiple.py
@@ -14,5 +14,8 @@ print("result: ", result)
 Y_predict = X.dot(result)
 
 R2 = 1 - ((Y - Y_predict)**2).sum() / ((Y - Y.mean())**2).sum()
-
 print("R2: ", R2)
+
+plt.scatter(Y, Y_predict)
+plt.plot([Y.min(), Y.max()], [Y.min(), Y.max()], c='r')
+plt.show()
